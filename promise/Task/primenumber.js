d200e0f6527c57cf99c8e0de81ec2b35ab4526e5
Akash Desai <103101706+Akashh2151@users.noreply.github.com>
2023-08-11 22:50:49 +0530

    update prime number program
    promise\Task\primenumber.js

--- promise/Task/primenumber.js
+++ promise/Task/primenumber.js
@@ -1,15 +1,20 @@
-function primenumber(num) {
-   
-   for (let index = 2; index <= 10; index++)
-    if (num % index == 0) {
-        console.log("its prime",num);
-        break
-    } else {
-        console.log("its not prime",num);
-        break  
-    }
-    
-}
-primenumber(11)   //invok function
-primenumber(21)   //invok function
 
+function isPrime(n) {
+    // Corner case
+    if (n <= 1)
+        return false;
+
+    // Check from 2 to n-1
+    for (let i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+// Driver Code
+
+isPrime(11) 
+isPrime(21) 
+    ? console.log("true") 
+    : console.log("false");
